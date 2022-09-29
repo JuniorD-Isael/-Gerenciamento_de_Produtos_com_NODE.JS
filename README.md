f7466976ba98b1d878e0d6865736a84e6251e788
JuniorD-Isael <104312466+JuniorD-Isael@users.noreply.github.com>
2022-09-28 22:30:40 -0300

    API finalizada

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # API_REST
-Desenvolvendo uma API com Node.JS
+Desenvolvendo uma API Node.JS com o framework Express
+Para simular um banco de dados a aplicação salva as informações em um arquivo JSON
+
+Temos as funções basicas que uma API pode precisar:
+POST => Inserir um dado
+Get => Buscar um/mais dados
+PUT => ALterar um dado
+DELETE => Remover um dado
--- app.js
+++ app.js
@@ -1,12 +1,21 @@
-const express = require('express')
+const express = require('express');
 const {request, response} = require("express");
-const { randomUUID } = require("crypto")
+const { randomUUID } = require("crypto");
+const fs = require("fs");
 
 const app = express();
 
 app.use(express.json());
 
-const products = [];
+let products = [];
+
+fs.readFile("products.json", "utf-8", (err, data) => {
+    if (err) {
+        console.log(err)
+    } else {
+        products = JSON.parse(data);
+    }
+});
 
 /**
  * POST => Inserir um dado
@@ -32,6 +41,8 @@ app.post('/products', (request, response) => {
 
     products.push(product);
 
+    productFile();
+
     return response.json(products);
 });
 
@@ -39,6 +50,48 @@ app.get("/products", (request, response) => {
     return response.json(products);
 });
 
+app.get("/products/:id", (request, response) => {
+    const {id} = request.params;
+    const product  = products.find(product => product.id === id);
+    return response.json(product)
+});
+
+app.put("/products/:id", (request, response) => {
+    const {id} = request.params;
+    const {name, price} = request.body;
+
+    const productIndex = products.findIndex((product) => product.id === id);
+    products[productIndex] = {
+        ...products[productIndex],
+        name,
+        price,
+    };
+    productFile();
+    return response.json({ message: "Produtor alterado com sucesso!" })
+});
+
+app.delete("/products/:id", (request, response) => {
+    const {id} = request.params;
+    const productIndex = products.findIndex((product) => product.id === id);
+
+    products.splice(productIndex, 1);
+    
+    productFile()
+
+    return response.json({ message: "Produto removido com sucesso!" })
+});
+
+// A função productFile() cria um arquivo JSON para simular um banco de dados.
+
+function productFile() {
+    fs.writeFile("products.json", JSON.stringify(products), (err) => {
+        if (err) {
+            console.log(err)
+        }else {
+            console.log("Produto inserido")
+        }
+    })
+}
 
 app.listen(4002, () => console.log('Servidor esta rodando na porta 4002'))
 
